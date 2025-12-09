0b3b707372404f50058298fefb6bfc70c9b1edb7
zachstanford <zachstanford@shopfront.com.au>
2025-12-10 09:04:17 +1100

    Created a new index file for the Emitable Events and Actions
    Updated the Sale index.ts file to export the SaleCreate types as their renamed types directly

---
+++ src/Actions/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./Button.js";
+export { Dialog } from "./Dialog.js";
+export { Redirect } from "./Redirect.js";
+export { SaleKey } from "./SaleKey.js";
+export { Toast, type ToastType } from "./Toast.js";
